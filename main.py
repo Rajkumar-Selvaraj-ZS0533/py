54105bb8cdbe64ddc72a024f9e18d2120150db88
Rajkumar-Selvaraj <148527647+Rajkumar-Selvaraj@users.noreply.github.com>
2024-02-03 15:34:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # hello.py
 
+import json
+
 def handler(event, context):
     print("hello world")
-    return {"statusCode": 200, "body": "Hello, World!"}
+    return {"statusCode": 200, "body": json.dumps({"message": "Hello, World!"})}
